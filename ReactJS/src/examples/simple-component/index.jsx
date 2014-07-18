84c4326061026b810efd5df89583a928188218e9
Gupta Garuda <gupta.garuda@pellucid.com>
2014-07-18 13:05:48 -0400

    add transitions

--- ReactJS/src/examples/simple-component/index.jsx
+++ ReactJS/src/examples/simple-component/index.jsx
@@ -1,12 +1,17 @@
     /** @jsx React.DOM */
+    var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
     var Ticker = React.createClass({
         render: function() {
-            return (<div key={this.props.ticker.symbol}>
-              <span>{this.props.ticker.symbol}</span>&nbsp;
-              <span>{(this.props.ticker.open).toFixed(2)}</span>&nbsp;
-              <span>{(this.props.ticker.open + this.props.ticker.dayChange).toFixed(2)}</span>&nbsp;
-              <span>{this.props.ticker.dayChange}</span>
-            </div>);
+            return (
+              <ReactCSSTransitionGroup key={this.props.ticker.symbol} transitionName="ticker">
+                  <div key={this.props.ticker.symbol} className={"ticker " + (this.props.ticker.dayChange > 0 ? 'up' : 'down')}>
+                    <span>{this.props.ticker.symbol}</span>&nbsp;
+                    <span>{(this.props.ticker.open).toFixed(2)}</span>&nbsp;
+                    <span>{(this.props.ticker.open + this.props.ticker.dayChange).toFixed(2)}</span>&nbsp;
+                    <span>{this.props.ticker.dayChange}</span>
+                  </div>
+              </ReactCSSTransitionGroup>
+            );
         },
 
         getDefaultProps: function () {
