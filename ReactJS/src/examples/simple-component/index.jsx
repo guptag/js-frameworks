52e692661d31ef1a0b0113ea124833a8beab6171
Gupta Garuda <gupta.garuda@pellucid.com>
2014-07-21 16:25:42 -0600

    styling tickers

--- ReactJS/src/examples/simple-component/index.jsx
+++ ReactJS/src/examples/simple-component/index.jsx
@@ -3,10 +3,9 @@
         render: function() {
             return (
               <div className={"ticker " + (this.props.ticker.dayChange > 0 ? 'up' : 'down')}>
-                <span>{this.props.ticker.symbol}</span>&nbsp;
-                <span>{(this.props.ticker.open).toFixed(2)}</span>&nbsp;
-                <span>{(this.props.ticker.open + this.props.ticker.dayChange).toFixed(2)}</span>&nbsp;
-                <span>{this.props.ticker.dayChange}</span>
+                <span className="symbol">{this.props.ticker.symbol}</span>
+                <span className="price">{"$" + (this.props.ticker.open + this.props.ticker.dayChange).toFixed(2)}</span>
+                <span className="change">{(this.props.ticker.dayChange >0 ? "+" : "-") + Math.abs(this.props.ticker.dayChange)}</span>
               </div>
             );
         },
@@ -48,7 +47,8 @@
         },
 
         componentDidMount: function () {
-          this.interval = setInterval(this.loadData, 2000);
+          this.loadData();
+          this.interval = setInterval(this.loadData, 5000);
         },
 
         componentWillUnmount: function () {
