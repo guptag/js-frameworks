6f86870a91451fe4d1b2eeafe02304159765d403
Gupta Garuda <gugarud@microsoft.com>
2017-07-27 17:59:56 -0700

    Sample todo app with redux and typescript

---
+++ Redux/examples/redux-perf-test/src/app/components/app.tsx
@@ -0,0 +1,46 @@
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import * as React from 'react';
+
+import {
+  addTodo,
+  editTodo,
+  clearCompleted,
+  completeAll,
+  completeTodo,
+  deleteTodo
+} from '../../todos/actions';
+
+import Header from '../../todos/components/header';
+import MainSection from '../../todos/components/mainSection';
+import * as model from '../../todos/models/todo';
+
+interface AppProps {
+  todos: model.Todo[];
+  dispatch: Dispatch<{}>;
+}
+
+class App extends React.Component<AppProps, void> {
+  render() {
+    const { todos, dispatch } = this.props;
+
+    return (
+      <div className="todoapp">
+        <Header addTodo={(text: string) => dispatch(addTodo(text))} />
+        <MainSection
+            todos={todos}
+            editTodo={(t,s) => dispatch(editTodo(t, s))}
+            deleteTodo={(t: model.Todo) => dispatch(deleteTodo(t))}
+            completeTodo={(t: model.Todo) => dispatch(completeTodo(t))}
+            clearCompleted={() => dispatch(clearCompleted())}
+            completeAll={() => dispatch(completeAll())}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+export default connect(mapStateToProps)(App);
